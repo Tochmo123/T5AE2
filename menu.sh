e2e42ff9169e4796121c636e22870b6c73b5dea4
Alejandro Tos <altove@floridauniversiratia.es>
2023-01-31 16:04:36 +0100

    Añadiendo el menu

---
+++ menu.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "-------------MENU D'OPCIONS:------------------"
+echo "Operació 1: JUGA EL AHORCADO AMB LA MAQUINA."
+echo "Operació 3: JUGA EL AHORCADO AMB UN ALTRE JUGADOR."
+echo "Operació 2: EIXIDA DEL PROGRAMA."
+echo "----------------------------------------------"
+echo -n "Introduiex una opció: "
+read opc
+while [ $opc -ne 3 ]
+do
+case $opc in 
+    1)
+        source solucion1.sh ;;
+    2)
+        source s2.sh ;;
+    *)
+        echo "Opció no valida." ;;
+esac
+echo -n "Introdueix altra opció: "
+read opc
+done
+echo "Fin del programa."
