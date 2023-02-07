971d4e2d42a85526789f9da397a64ef5647a8d40
Tochmo123 <114494835+Tochmo123@users.noreply.github.com>
2023-02-07 16:14:50 +0100

    Mas sleeps

--- jugador.sh
+++ jugador.sh
@@ -209,7 +209,7 @@ echo "  | |  _    / _ \   |  \| |   / _ \   \___ \   | |   |  _|   "
 echo "  | |_| |  / ___ \  | |\  |  / ___ \   ___) |  | |   | |___  "
 echo "   \____| /_/   \_\ |_| \_| /_/   \_\ |____/   |_|   |_____| "
 echo -e "Palabra usada:$palabra \nIntentos sobrantes:$intentos " >> registro.txt
-sleep 3
+sleep 5
 else 
 echo "   ____   _____  ____   ____    ___   _____     _     "
 echo "  |  _ \ | ____||  _ \ |  _ \  / _ \ |_   _|   / \    "
@@ -219,5 +219,5 @@ echo "  |____/ |_____||_| \_\|_| \_\ \___/   |_|  /_/   \_\ "
 echo
 echo "La palabra era: $palabra"
 echo -e "Palabra fallada:$palabra" >> registro.txt
-sleep 3
+sleep 5
 fi
